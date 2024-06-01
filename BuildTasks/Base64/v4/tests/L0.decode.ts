e1a5eed28784f70986891e29f8424fc4ba144834
Alan S. Ferreira <alansferreira1984@gmail.com>
2024-06-01 16:48:18 -0300

    chore(release): 4.3.6

--- BuildTasks/Base64/v4/tests/L0.decode.ts
+++ BuildTasks/Base64/v4/tests/L0.decode.ts
@@ -1,7 +1,6 @@
 import { join as joinPath} from 'node:path';
 import { TaskMockRunner } from 'azure-pipelines-task-lib/mock-run';
-import { setIn } from '../../../Common/v4/ParamsUtil';
-import { EXT } from '../../../Common/v4/RuntimeUtil';
+import { setIn, EXT } from '@alell/azure-pipelines-task-commons';
 import { _loadData } from 'azure-pipelines-task-lib/internal'
 
 setIn({
